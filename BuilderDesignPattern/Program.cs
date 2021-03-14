df64f0d321d872b95623100bcddf055249079c85
Aya AbdulSlam Hemaid <aya.abdel.slam93@gmail.com>
2021-03-14 12:30:07 +0200

    Initial Create

---
+++ BuilderDesignPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BuilderDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create shop with vehicle builders
+            VehicleBuilder builder;
+
+            Shop shop = new Shop();
+
+            // Construct and display vehicles
+
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BuilderDesignPattern/Shop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderDesignPattern
+{
+    public class Shop
+    {
+        // Builder uses a complex series of steps
+
+        public void Construct(VehicleBuilder vehicleBuilder)
+        {
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
+        }
+    }
+}
